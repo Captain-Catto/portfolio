8301757b32c49fa9211183f7949e6a17c5c5b8ba
Trí Đạt Lê Quang <lequangtridat2000@gmail.com>
2025-06-19 19:40:07 +0700

    sửa lại icon ở thanh tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -77,8 +77,8 @@ export default function RootLayout({
       <head>
         <meta name="theme-color" content="#000000" />
         <meta name="msapplication-TileColor" content="#000000" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" href="/favicon.ico" type="image/png" />
+        <link rel="apple-touch-icon" href="/favicon.ico" />
       </head>
       <body className={inter.className}>
         <LanguageProvider>{children}</LanguageProvider>
